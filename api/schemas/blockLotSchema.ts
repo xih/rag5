d0637233b9f67bff8f3df8679c4ff88fee0f3c7a
Dennis <dennis@momentum.local>
2024-05-13 13:05:28 -0700

    get oneBlockAndLot result from the sf database

---
+++ api/schemas/blockLotSchema.ts
@@ -0,0 +1,49 @@
+// this is the schema for fetching this url
+// https://recorder.sfgov.org/SearchService/api/Search/GetSearchResults?APN=&Acres=&AddressLineOne=&AddressLineTwo=&AreaCode=&Block=0001&BookNo=&BookType=&Building=&BuildingHigh=&BuildingLow=&ChildDocumentGlobalID=&City=&CityArea=&CityPhase=&CityVillageCode=&Code=&CornerLetter=&CornerNumber=&CountryCode=&County=&CountyCode=&Division=&DocNumberFrom=&DocNumberTo=&DocumentClass=OfficialRecords&EventDateFrom=&EventDateTo=&FilingCode=&FilingCodeGroupID=&FilingCodeRefiners=&FirstName=&FirstQtrSect=&FrameNumberFrom=&FrameNumberThrough=&GarageHigh=&GarageLow=&GovernmentUnit=&HighLot=&Info=&IsBasicSearch=false&IsExactMatch=&LastName=&LegalDescription=&LotTract=&LotType=&LowLot=001&MaxRecordedDate=05%2F13%2F2024&MiddleName=&MinRecordedDate=12%2F28%2F1989&Name=&NameRefiners=&NameTypeID=0&NotInSidwellFl=&NumAcres=&OneHalfCode=&PageNo=&PageNumberFrom=&PageNumberThrough=&ParentDocumentGlobalID=&PartOneCode=&PartTwoCode=&Pin=&PlatName=&PrimaryFirstName=&PrimaryLastName=&PrimaryMiddleName=&ProfileID=Public&PropertyID=&PropertyPhase=&PropertyUnit=&Range=&RangeDirection=&RefinementTokens=&RollNumber=&RollType=&Rows=10&SearchText=&SecDocNumberFrom=&SecDocNumberTo=&SecondQtrSect=&SecondaryFirstName=&SecondaryLastName=&SecondaryMiddleName=&Section=&SectionHalf=&SectionLot=&SectionQuarters=&SheetNumberFrom=&SheetNumberThrough=&SortFields=&SortOrders=&SplitUnit=&StartRow=0&State=&Suffix=&TertiaryFirstName=&TertiaryLastName=&TertiaryMiddleName=&ThirdQtrSect=&TownCode=&TownDirection=&TownhomeID=&Township=&UnderPin=&YearRefiners=&ZipCode=&ZipCodeFour=
+
+import { z } from "zod";
+
+const searchResultSchema = z.object({
+  ID: z.string(),
+  PrimaryDocNumber: z.string(),
+  DocumentDate: z.string(),
+  FilingCode: z.string(),
+  Names: z.string(),
+  SecondaryDocNumber: z.string(),
+  BookType: z.string(),
+  BookNumber: z.string(),
+  NumberOfPages: z.string(),
+});
+
+const yearSchema = z.object({
+  Key: z.string(),
+  Value: z.number(),
+  ID: z.string(),
+  RefinementToken: z.null().optional(),
+});
+
+const filingCodeSchema = z.object({
+  Key: z.string(),
+  Value: z.number(),
+  ID: z.string(),
+  RefinementToken: z.null().optional(),
+});
+
+const nameSchema = z.object({
+  Key: z.string(),
+  Value: z.number(),
+  ID: z.string(),
+  RefinementToken: z.null().optional(),
+});
+
+const refinementPanelDataSchema = z.object({
+  Years: z.array(yearSchema),
+  FilingCodes: z.array(filingCodeSchema),
+  Names: z.array(nameSchema),
+});
+
+export const blockLotSearchResultsSchema = z.object({
+  ResultCount: z.number(),
+  SearchResults: z.array(searchResultSchema),
+  RefinementPanelData: refinementPanelDataSchema,
+});
--- api/scripts/scrape.ts
+++ api/scripts/scrape.ts
@@ -5,7 +5,16 @@
 // 3. site: https://recorder.sfgov.org/#!/simple
 // 4. site: https://sfplanninggis.org/pim/?pub=true to find the block/lot numbers
 // 5. site: https://data.sfgov.org/Housing-and-Buildings/Assessor-Historical-Secured-Property-Tax-Rolls/wv5m-vpq2/data_preview
+// 6. get the secureKey
+// 7. get oneBlockAndLot
+// 8. [TODO]: get back names for results
+// 9. [TODO]: getNames
+// 10. [TODO]: store the assessor-historical-secured-property-tax-rolls into a database and load the missing ones
+// 11. [TODO]: write to sqlite database, then write to supabase database
+
 import z from "zod";
+import { blockLotSearchResultsSchema } from "../schemas/blockLotSchema";
+import _ from "underscore";
 
 const userAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36`;
 const cookie = `googtrans=/en/en; BIGipServerASR-102_recorder.sfgov.org_PRD_EXT_pool=2160622032.20480.0000; HideDetails=0`;
@@ -65,9 +74,75 @@ async function getSecureKey() {
   };
 }
 
+async function getOneBlockLot(
+  { block, lot }: { block: string; lot: string },
+  key: encryptedKeyAndPassword
+) {
+  // fetch(
+  //   "https://recorder.sfgov.org/SearchService/api/Search/GetSearchResults?APN=&Acres=&AddressLineOne=&AddressLineTwo=&AreaCode=&Block=0001&BookNo=&BookType=&Building=&BuildingHigh=&BuildingLow=&ChildDocumentGlobalID=&City=&CityArea=&CityPhase=&CityVillageCode=&Code=&CornerLetter=&CornerNumber=&CountryCode=&County=&CountyCode=&Division=&DocNumberFrom=&DocNumberTo=&DocumentClass=OfficialRecords&EventDateFrom=&EventDateTo=&FilingCode=&FilingCodeGroupID=&FilingCodeRefiners=&FirstName=&FirstQtrSect=&FrameNumberFrom=&FrameNumberThrough=&GarageHigh=&GarageLow=&GovernmentUnit=&HighLot=&Info=&IsBasicSearch=false&IsExactMatch=&LastName=&LegalDescription=&LotTract=&LotType=&LowLot=001&MaxRecordedDate=05%2F13%2F2024&MiddleName=&MinRecordedDate=12%2F28%2F1989&Name=&NameRefiners=&NameTypeID=0&NotInSidwellFl=&NumAcres=&OneHalfCode=&PageNo=&PageNumberFrom=&PageNumberThrough=&ParentDocumentGlobalID=&PartOneCode=&PartTwoCode=&Pin=&PlatName=&PrimaryFirstName=&PrimaryLastName=&PrimaryMiddleName=&ProfileID=Public&PropertyID=&PropertyPhase=&PropertyUnit=&Range=&RangeDirection=&RefinementTokens=&RollNumber=&RollType=&Rows=10&SearchText=&SecDocNumberFrom=&SecDocNumberTo=&SecondQtrSect=&SecondaryFirstName=&SecondaryLastName=&SecondaryMiddleName=&Section=&SectionHalf=&SectionLot=&SectionQuarters=&SheetNumberFrom=&SheetNumberThrough=&SortFields=&SortOrders=&SplitUnit=&StartRow=0&State=&Suffix=&TertiaryFirstName=&TertiaryLastName=&TertiaryMiddleName=&ThirdQtrSect=&TownCode=&TownDirection=&TownhomeID=&Township=&UnderPin=&YearRefiners=&ZipCode=&ZipCodeFour=",
+  //   {
+  //     headers: {
+  //       accept: "application/json, text/plain, */*",
+  //       "accept-language": "en-US,en;q=0.9",
+  //       authorization: "Bearer",
+  //       encryptedkey:
+  //         "VBdnaiUBTpGZapAKGiLz1sd2oG9TOPw8fxRIWcJgWJnDh3cXXY89HbxtHf/xvDSky7AL/3rbuPHFpO15rLSZyC/bYFvOE922a7uponbag00pphILCTMBE2Ga+D6UC9+jjQwXmbHZTfpnDEn6Db1pZktlKMCDlDqw3Sn/wpSM4JI=",
+  //       password: "MTIwMDMxMDA4OA==",
+  //       "sec-ch-ua":
+  //         '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+  //       "sec-ch-ua-mobile": "?0",
+  //       "sec-ch-ua-platform": '"macOS"',
+  //       "sec-fetch-dest": "empty",
+  //       "sec-fetch-mode": "cors",
+  //       "sec-fetch-site": "same-origin",
+  //       cookie:
+  //         "googtrans=/en/en; BIGipServerASR-102_recorder.sfgov.org_PRD_EXT_pool=2160622032.20480.0000; HideDetails=0",
+  //       Referer: "https://recorder.sfgov.org/",
+  //       "Referrer-Policy": "strict-origin-when-cross-origin",
+  //     },
+  //     body: null,
+  //     method: "GET",
+  //   }
+  // );
+  const res = await fetch(
+    `https://recorder.sfgov.org/SearchService/api/Search/GetSearchResults?APN=&Acres=&AddressLineOne=&AddressLineTwo=&AreaCode=&\
+      Block=${block}&BookNo=&BookType=&Building=&BuildingHigh=&BuildingLow=&ChildDocumentGlobalID=&City=&CityArea=&CityPhase=&CityVillageCode=&Code=&CornerLetter=&CornerNumber=&CountryCode=&County=&CountyCode=&Division=&DocNumberFrom=&DocNumberTo=&DocumentClass=OfficialRecords&EventDateFrom=&EventDateTo=&FilingCode=&FilingCodeGroupID=&FilingCodeRefiners=&FirstName=&FirstQtrSect=&FrameNumberFrom=&FrameNumberThrough=&GarageHigh=&GarageLow=&GovernmentUnit=&HighLot=&Info=&IsBasicSearch=false&IsExactMatch=&LastName=&LegalDescription=&LotTract=&LotType=&\
+      LowLot=${lot}&MaxRecordedDate=05%2F13%2F2024&MiddleName=&MinRecordedDate=12%2F28%2F1989&Name=&NameRefiners=&NameTypeID=0&NotInSidwellFl=&NumAcres=&OneHalfCode=&PageNo=&PageNumberFrom=&PageNumberThrough=&ParentDocumentGlobalID=&PartOneCode=&PartTwoCode=&Pin=&PlatName=&PrimaryFirstName=&PrimaryLastName=&PrimaryMiddleName=&ProfileID=Public&PropertyID=&PropertyPhase=&PropertyUnit=&Range=&RangeDirection=&RefinementTokens=&RollNumber=&RollType=&Rows=10&SearchText=&SecDocNumberFrom=&SecDocNumberTo=&SecondQtrSect=&SecondaryFirstName=&SecondaryLastName=&SecondaryMiddleName=&Section=&SectionHalf=&SectionLot=&SectionQuarters=&SheetNumberFrom=&SheetNumberThrough=&SortFields=&SortOrders=&SplitUnit=&StartRow=0&State=&Suffix=&TertiaryFirstName=&TertiaryLastName=&TertiaryMiddleName=&ThirdQtrSect=&TownCode=&TownDirection=&TownhomeID=&Township=&UnderPin=&YearRefiners=&ZipCode=&ZipCodeFour=`,
+    {
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        Authorization: "Bearer",
+        "User-Agent": userAgent,
+        Referer: "https://recorder.sfgov.org/",
+        Cookie: cookie,
+        EncryptedKey: key.encryptedKey,
+        Password: key.password,
+      },
+    }
+  );
+
+  const data = await res.json();
+
+  const blockLotSearchResults = blockLotSearchResultsSchema.parse(data);
+
+  if (blockLotSearchResults.SearchResults.length === 0) {
+    return null;
+  }
+
+  const firstResult = _.first(blockLotSearchResults.SearchResults);
+
+  return {
+    latestResult: {
+      firstResult,
+    },
+    ...blockLotSearchResults,
+  };
+}
+
 async function main() {
   const key = await getSecureKey();
-  console.log(key, "key");
+  const result = await getOneBlockLot({ block: "0001", lot: "001" }, key);
+  console.log(result?.latestResult);
 }
 
 main();
