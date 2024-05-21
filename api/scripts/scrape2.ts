e86b20ec5bcab8371aa3f09a10af01a52a66a3aa
Dennis <dennis@momentum.local>
2024-05-21 10:35:57 -0700

    write getBackOneLot successfully

--- api/scripts/scrape2.ts
+++ api/scripts/scrape2.ts
@@ -2,29 +2,13 @@
 // 1. get the secure key
 // 2. call the
 
+import { blockLotSearchResultsSchema } from "../schemas/blockLotSchema";
 import { accessorRecorderEncryptedKeySchema } from "../schemas/encryptedKeySchema";
 
 const userAgent =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
 const cookie =
   "googtrans=/en/en; BIGipServerASR-102_recorder.sfgov.org_PRD_EXT_pool=2160622032.20480.0000; HideDetails=0";
-
-// curl 'https://recorder.sfgov.org/SearchService/api/SearchConfiguration/GetSecureKey' \
-// -H 'Accept: application/json, text/plain, */*' \
-// -H 'Accept-Language: en-US,en;q=0.9' \
-// -H 'Authorization: Bearer' \
-// -H 'Connection: keep-alive' \
-// -H 'Cookie: googtrans=/en/en; BIGipServerASR-102_recorder.sfgov.org_PRD_EXT_pool=2160622032.20480.0000; HideDetails=0' \
-// -H 'EncryptedKey: TlDWDwNq/IAmFKB1jDKraS99S4qn34WxuRoK5Zq+uacbflB8smU7siLEh1IQ8JSfbq2qDZpwZP8JDDP19kjqD7CHREGcHKvBit3t7xKQkOMkpRWVD3Wq5WJVbTnCLm4k/FqB7xvFjo7SF0cZDyYi94dyAB+RmLP2jdC1XYiQa+M=' \
-// -H 'Password: ODM2MDg2Mjg3' \
-// -H 'Referer: https://recorder.sfgov.org/' \
-// -H 'Sec-Fetch-Dest: empty' \
-// -H 'Sec-Fetch-Mode: cors' \
-// -H 'Sec-Fetch-Site: same-origin' \
-// -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' \
-// -H 'sec-ch-ua: "Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"' \
-// -H 'sec-ch-ua-mobile: ?0' \
-// -H 'sec-ch-ua-platform: "macOS"'
 
 async function getSecureKey() {
   const res = await fetch(
@@ -51,9 +35,50 @@ async function getSecureKey() {
   };
 }
 
+type SecureKey = {
+  encryptedKey: string;
+  password: string;
+};
+
+async function getOneBlockLot(
+  { block, lot }: { block: string; lot: string },
+  secureKey: SecureKey
+) {
+  const res = await fetch(
+    `https://recorder.sfgov.org/SearchService/api/Search/GetSearchResults?APN=&Acres=&AddressLineOne=&AddressLineTwo=&AreaCode=&\
+  Block=${block}&BookNo=&BookType=&Building=&BuildingHigh=&BuildingLow=&ChildDocumentGlobalID=&City=&CityArea=&CityPhase=&CityVillageCode=&Code=&CornerLetter=&CornerNumber=&CountryCode=&County=&CountyCode=&Division=&DocNumberFrom=&DocNumberTo=&DocumentClass=OfficialRecords&EventDateFrom=&EventDateTo=&FilingCode=&FilingCodeGroupID=&FilingCodeRefiners=&FirstName=&FirstQtrSect=&FrameNumberFrom=&FrameNumberThrough=&GarageHigh=&GarageLow=&GovernmentUnit=&HighLot=&Info=&IsBasicSearch=false&IsExactMatch=&LastName=&LegalDescription=&LotTract=&LotType=&\
+  LowLot=${lot}&MaxRecordedDate=05%2F21%2F2024&MiddleName=&MinRecordedDate=12%2F28%2F1989&Name=&NameRefiners=&NameTypeID=0&NotInSidwellFl=&NumAcres=&OneHalfCode=&PageNo=&PageNumberFrom=&PageNumberThrough=&ParentDocumentGlobalID=&PartOneCode=&PartTwoCode=&Pin=&PlatName=&PrimaryFirstName=&PrimaryLastName=&PrimaryMiddleName=&ProfileID=Public&PropertyID=&PropertyPhase=&PropertyUnit=&Range=&RangeDirection=&RefinementTokens=&RollNumber=&RollType=&Rows=10&SearchText=&SecDocNumberFrom=&SecDocNumberTo=&SecondQtrSect=&SecondaryFirstName=&SecondaryLastName=&SecondaryMiddleName=&Section=&SectionHalf=&SectionLot=&SectionQuarters=&SheetNumberFrom=&SheetNumberThrough=&SortFields=&SortOrders=&SplitUnit=&StartRow=0&State=&Suffix=&TertiaryFirstName=&TertiaryLastName=&TertiaryMiddleName=&ThirdQtrSect=&TownCode=&TownDirection=&TownhomeID=&Township=&UnderPin=&YearRefiners=&ZipCode=&ZipCodeFour=`,
+    {
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        Authorization: "Bearer",
+        Connection: "keep-alive",
+        Referer: "https://recorder.sfgov.org/",
+        "User-Agent": userAgent,
+        Cookie: cookie,
+        EncryptedKey: secureKey.encryptedKey,
+        Password: secureKey.password,
+      },
+    }
+  );
+
+  const data = await res.json();
+
+  const blockLotData = blockLotSearchResultsSchema.parse(data);
+
+  console.log(blockLotData);
+}
+
 async function main() {
-  const data = await getSecureKey();
-  console.log(data);
+  const key = await getSecureKey();
+
+  const blockLot = await getOneBlockLot(
+    {
+      block: "0001",
+      lot: "001",
+    },
+    key
+  );
 }
 
 main();
