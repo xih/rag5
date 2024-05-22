b4ca2849b646cd70130e9bf574511d71091a6edd
Dennis <dennis@momentum.local>
2024-05-22 09:24:29 -0700

    correctly get the first 10 documents from a specific block and lot

--- api/scripts/scrape2.ts
+++ api/scripts/scrape2.ts
@@ -1,6 +1,14 @@
 // [5-21-2024] Rewrite the script from scratch
 // 1. get the secure key, getOneBlock
 // 2. get a list of searchResultIds and call the documentAPI with that
+// [5-22-2024]
+// 3. combine the document with blocklotData and write it as a row in the database
+// 4. make a separate table that has a list of all the property owners that has a many to one relationship
+// to documents
+// 5. block - 0001, lot - 001 -> APN: 0001001 (add this as a column to the database)
+// 6. [todo: ] reverse engineer this:
+// https://recorder.sfgov.org/SearchService/api/search/GetDocumentDetails/5703079
+// 7. bug on fetch
 
 import { blockLotSearchResultsSchema } from "../schemas/blockLotSchema";
 import { accessorRecorderEncryptedKeySchema } from "../schemas/encryptedKeySchema";
@@ -46,9 +54,20 @@ async function getOneBlockLot(
   secureKey: SecureKey
 ) {
   const res = await fetch(
-    `https://recorder.sfgov.org/SearchService/api/Search/GetSearchResults?APN=&Acres=&AddressLineOne=&AddressLineTwo=&AreaCode=&\
-  Block=${block}&BookNo=&BookType=&Building=&BuildingHigh=&BuildingLow=&ChildDocumentGlobalID=&City=&CityArea=&CityPhase=&CityVillageCode=&Code=&CornerLetter=&CornerNumber=&CountryCode=&County=&CountyCode=&Division=&DocNumberFrom=&DocNumberTo=&DocumentClass=OfficialRecords&EventDateFrom=&EventDateTo=&FilingCode=&FilingCodeGroupID=&FilingCodeRefiners=&FirstName=&FirstQtrSect=&FrameNumberFrom=&FrameNumberThrough=&GarageHigh=&GarageLow=&GovernmentUnit=&HighLot=&Info=&IsBasicSearch=false&IsExactMatch=&LastName=&LegalDescription=&LotTract=&LotType=&\
-  LowLot=${lot}&MaxRecordedDate=05%2F21%2F2024&MiddleName=&MinRecordedDate=12%2F28%2F1989&Name=&NameRefiners=&NameTypeID=0&NotInSidwellFl=&NumAcres=&OneHalfCode=&PageNo=&PageNumberFrom=&PageNumberThrough=&ParentDocumentGlobalID=&PartOneCode=&PartTwoCode=&Pin=&PlatName=&PrimaryFirstName=&PrimaryLastName=&PrimaryMiddleName=&ProfileID=Public&PropertyID=&PropertyPhase=&PropertyUnit=&Range=&RangeDirection=&RefinementTokens=&RollNumber=&RollType=&Rows=10&SearchText=&SecDocNumberFrom=&SecDocNumberTo=&SecondQtrSect=&SecondaryFirstName=&SecondaryLastName=&SecondaryMiddleName=&Section=&SectionHalf=&SectionLot=&SectionQuarters=&SheetNumberFrom=&SheetNumberThrough=&SortFields=&SortOrders=&SplitUnit=&StartRow=0&State=&Suffix=&TertiaryFirstName=&TertiaryLastName=&TertiaryMiddleName=&ThirdQtrSect=&TownCode=&TownDirection=&TownhomeID=&Township=&UnderPin=&YearRefiners=&ZipCode=&ZipCodeFour=`,
+    // `https://recorder.sfgov.org/SearchService/api/Search/GetSearchResults?APN=&Acres=&AddressLineOne=&AddressLineTwo=&AreaCode=&Block=${encodeURIComponent(
+    //   block
+    // )}&BookNo=&BookType=&Building=&BuildingHigh=&BuildingLow=&ChildDocumentGlobalID=&City=&CityArea=&CityPhase=&CityVillageCode=&Code=&CornerLetter=&CornerNumber=&CountryCode=&County=&CountyCode=&Division=&DocNumberFrom=&DocNumberTo=&DocumentClass=OfficialRecords&EventDateFrom=&EventDateTo=&FilingCode=001&FilingCodeGroupID=&FilingCodeRefiners=&FirstName=&FirstQtrSect=&FrameNumberFrom=&FrameNumberThrough=&GarageHigh=&GarageLow=&GovernmentUnit=&HighLot=&Info=&IsBasicSearch=false&IsExactMatch=&LastName=&LegalDescription=&LotTract=&LotType=&LowLot=${encodeURIComponent(
+    //   lot
+    // )}&MaxRecordedDate=${encodeURIComponent(
+    //   "05/22/2024"
+    // )}&MiddleName=&MinRecordedDate=${encodeURIComponent(
+    //   "12/28/1989"
+    // )}&Name=&NameRefiners=&NameTypeID=0&NotInSidwellFl=&NumAcres=&OneHalfCode=&PageNo=&PageNumberFrom=&PageNumberThrough=&ParentDocumentGlobalID=&PartOneCode=&PartTwoCode=&Pin=&PlatName=&PrimaryFirstName=&PrimaryLastName=&PrimaryMiddleName=&ProfileID=Public&PropertyID=&PropertyPhase=&PropertyUnit=&Range=&RangeDirection=&RefinementTokens=&RollNumber=&RollType=&Rows=20&SearchText=&SecDocNumberFrom=&SecDocNumberTo=&SecondQtrSect=&SecondaryFirstName=&SecondaryLastName=&SecondaryMiddleName=&Section=&SectionHalf=&SectionLot=&SectionQuarters=&SheetNumberFrom=&SheetNumberThrough=&SortFields=&SortOrders=&SplitUnit=&StartRow=0&State=&Suffix=&TertiaryFirstName=&TertiaryLastName=&TertiaryMiddleName=&ThirdQtrSect=&TownCode=&TownDirection=&TownhomeID=&Township=&UnderPin=&YearRefiners=&ZipCode=&ZipCodeFour=`,
+    `https://recorder.sfgov.org/SearchService/api/Search/GetSearchResults?APN=&Acres=&AddressLineOne=&AddressLineTwo=&AreaCode=&Block=${encodeURIComponent(
+      block
+    )}&BookNo=&BookType=&Building=&BuildingHigh=&BuildingLow=&ChildDocumentGlobalID=&City=&CityArea=&CityPhase=&CityVillageCode=&Code=&CornerLetter=&CornerNumber=&CountryCode=&County=&CountyCode=&Division=&DocNumberFrom=&DocNumberTo=&DocumentClass=OfficialRecords&EventDateFrom=&EventDateTo=&FilingCode=&FilingCodeGroupID=&FilingCodeRefiners=&FirstName=&FirstQtrSect=&FrameNumberFrom=&FrameNumberThrough=&GarageHigh=&GarageLow=&GovernmentUnit=&HighLot=&Info=&IsBasicSearch=false&IsExactMatch=&LastName=&LegalDescription=&LotTract=&LotType=&LowLot=${encodeURIComponent(
+      lot
+    )}&MaxRecordedDate=05%2F22%2F2024&MiddleName=&MinRecordedDate=12%2F28%2F1989&Name=&NameRefiners=&NameTypeID=0&NotInSidwellFl=&NumAcres=&OneHalfCode=&PageNo=&PageNumberFrom=&PageNumberThrough=&ParentDocumentGlobalID=&PartOneCode=&PartTwoCode=&Pin=&PlatName=&PrimaryFirstName=&PrimaryLastName=&PrimaryMiddleName=&ProfileID=Public&PropertyID=&PropertyPhase=&PropertyUnit=&Range=&RangeDirection=&RefinementTokens=&RollNumber=&RollType=&Rows=10&SearchText=&SecDocNumberFrom=&SecDocNumberTo=&SecondQtrSect=&SecondaryFirstName=&SecondaryLastName=&SecondaryMiddleName=&Section=&SectionHalf=&SectionLot=&SectionQuarters=&SheetNumberFrom=&SheetNumberThrough=&SortFields=&SortOrders=&SplitUnit=&StartRow=0&State=&Suffix=&TertiaryFirstName=&TertiaryLastName=&TertiaryMiddleName=&ThirdQtrSect=&TownCode=&TownDirection=&TownhomeID=&Township=&UnderPin=&YearRefiners=&ZipCode=&ZipCodeFour=`,
     {
       headers: {
         Accept: "application/json, text/plain, */*",
@@ -67,6 +86,11 @@ async function getOneBlockLot(
 
   const blockLotData = blockLotSearchResultsSchema.parse(data);
 
+  console.log(
+    blockLotData.SearchResults.length,
+    "22. What is the length here?"
+  );
+
   // console.log(blockLotData);
   return blockLotData;
 }
@@ -76,7 +100,9 @@ async function getNamesOnDocumentFromSearchResultId(
   secureKey: SecureKey
 ) {
   const res = await fetch(
-    `https://recorder.sfgov.org/SearchService/api/search/GetNamesForPagination/11070863/1/20`,
+    `https://recorder.sfgov.org/SearchService/api/search/GetNamesForPagination/${encodeURIComponent(
+      searchResultId
+    )}/1/20`,
     {
       headers: {
         Accept: "application/json, text/plain, */*",
@@ -122,13 +148,13 @@ interface DataObject {
   TotalNamesCount: number;
 }
 
-async function main() {
+async function main(block: string, lot: string) {
   const key = await getSecureKey();
 
   const blockLotData = await getOneBlockLot(
     {
-      block: "0001",
-      lot: "001",
+      block,
+      lot,
     },
     key
   );
@@ -170,13 +196,20 @@ async function main() {
 
     const { ID, ...newSearchResultWithoutId } = searchResult;
 
-    const searchResult2 = { ...newSearchResultWithoutId, ...result }; // Spread result into searchResult
+    const searchResult2 = {
+      ...newSearchResultWithoutId,
+      ...result,
+      block,
+      lot,
+      APN: `${block}${lot}`,
+    }; // Spread result into searchResult
 
     combinedResults.push(searchResult2); // Add searchResult to the combinedResults array
   }
 
   console.log(combinedResults);
   // combinedResults is one Document
+  // console.log("blockLotData", blockLotData);
 }
 
-main();
+main("0001", "001");
